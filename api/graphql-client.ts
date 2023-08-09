7bbe3808597dd0bf092c12377623f456ec6070a8
shauberdonArtefact <sacha.hauberdon@artefact.com>
2023-08-09 16:45:03 +0200

    add cache refresh

--- api/graphql-client.ts
+++ api/graphql-client.ts
@@ -4,5 +4,5 @@ export const WORDPRESS_API_URL = process.env.WORDPRESS_API_URL!;
 
 export const gqlClient = new GraphQLClient(WORDPRESS_API_URL, {
   fetch,
-  cache: "no-cache",
+  next: { revalidate: 10 },
 });
